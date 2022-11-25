f00036c1f2dc29f46c91afd9d972d3773552e7c3
MatiasMedina02 <115422568+MatiasMedina02@users.noreply.github.com>
2022-11-25 16:51:26 -0300

    Add files via upload

---
+++ presentacion.js
@@ -0,0 +1,18 @@
+const lista1 = [
+    "Matias",
+    20,
+    true,
+]
+
+const fecha_nacimiento = new Date ("June 18 2002");
+
+const libro_favorito = {
+    titulo: "Padre Rico Padre Pobre",
+    autor: "Robert Kiyosaki",
+    fecha: 2004,
+    url: "https://www.amazon.com/-/es/Robert-T-Kiyosaki/dp/160396181X"
+}
+
+const presentacion = [lista1, fecha_nacimiento, libro_favorito]
+
+console.log(presentacion);
